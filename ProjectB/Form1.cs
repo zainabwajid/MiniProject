371aadbda9f89b7879e0824655535bca24d3747b
zainabwajid <zainabwajid14@gmail.com>
2019-03-11 09:36:27 +0500

    Project Started

---
+++ ProjectB/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String conURL = "Data Source = (local);Initial Catalog = ProjectB; Integrated Security = True;MultipleActiveResultSets = True";
+            SqlConnection conn = new SqlConnection(conURL);
+            String cmd = "SELECT * FROM Student";
+            SqlCommand command = new SqlCommand(cmd, conn);
+            command.Parameters.Add(new SqlParameter("0", 1));
+            conn.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0] + " " + reader[1]);
+            }
+        }
+    }
+}
